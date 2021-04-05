28f9f9f23c0ef61addab930ccd08fcc0955739bf
Francisco Arenas <wook82@gmail.com>
2021-04-05 01:25:57 -0400

    solution 19

--- src/main.go
+++ src/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/w00k/Go.projecteuler.net/src/utility"
 )
 
 func main() {
-	fmt.Println(" ===> ", utility.Exc018())
+	fmt.Println(" ===> ", utility.Exc019())
 }
---
+++ src/utility/e019.go
@@ -0,0 +1,29 @@
+package utility
+
+import (
+	"fmt"
+	"time"
+)
+
+// add 1 day to t variable, start on 1902-01-01
+func addDay(t time.Time) time.Time {
+	return t.Add(time.Hour * 24)
+}
+
+// How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+func Exc019() int {
+	str := "1901-01-01T10:45:00.371Z"
+	endStr := "2000-12-31T10:45:00.371Z"
+	t, _ := time.Parse(time.RFC3339, str)
+	end, _ := time.Parse(time.RFC3339, endStr)
+	count := 0
+
+	for t.Unix() <= end.Unix() {
+		t = addDay(t)
+		if t.Day() == 1 && t.Weekday().String() == "Sunday" {
+			count += 1
+		}
+	}
+
+	return count
+}
